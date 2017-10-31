307cdcfc44a62e0db3023e2fb5b47ad775f11bc7
Zoombie <a13530836164@163.com>
2017-10-31 17:33:41 +0800

    Initial Commit

---
+++ Search/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Search
+//
+//  Created by WXL-TECH on 2017/10/31.
+//  Copyright © 2017年 WXL-TECH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Search/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Search
+//
+//  Created by WXL-TECH on 2017/10/31.
+//  Copyright © 2017年 WXL-TECH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
